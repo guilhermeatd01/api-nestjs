5318cef2f23c28ac25a02702b055b8416bd951af
Guilherme Fonseca <guilherme.atd@hotmail.com>
2020-10-01 07:54:08 -0300

    Implementação módulo Profile

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,27 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Profile } from './profile/profile.entity';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot()
+    TypeOrmModule.forRoot({
+      "type": "mysql",
+      "host": "db",
+      "port": 3306,
+      "username": "root",
+      "password": "root",
+      "database": "lazaros",
+      "synchronize": true,
+      "entities": [Profile],
+      "migrationsTableName": "migrations",
+      "migrations": ["src/migrations/*.js"],
+      "cli": {
+          "migrationsDir": "src/migrations"
+      }
+    }),
+    ProfileModule
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/profile/profile.controller.ts
@@ -0,0 +1,41 @@
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult, Repository } from 'typeorm';
+import { Profile } from './profile.entity';
+
+@Controller('profiles')
+export class ProfileController {
+  constructor(
+    @InjectRepository(Profile)
+    private profileRepository: Repository<Profile>,
+  ) {}
+
+  @Get()
+  async index(): Promise<Profile[]> {
+    return this.profileRepository.find();
+  }
+
+  @Get(':id')
+  async show(@Param('id') id: string): Promise<Profile> {
+    return this.profileRepository.findOneOrFail(id);
+  }
+
+  @Post()
+  async store(@Body() body: Profile): Promise<Profile> {
+    const profile = this.profileRepository.create(body);
+    return this.profileRepository.save(profile);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() body: Profile): Promise<Profile> {
+    await this.profileRepository.findOneOrFail(id);
+    await this.profileRepository.update({id: +id}, body);
+    return await this.profileRepository.findOneOrFail(id);
+  }
+
+  @Delete(':id')
+  async destroy(@Param('id') id: string): Promise<DeleteResult> {
+    await this.profileRepository.findOneOrFail(id);
+    return this.profileRepository.delete(id);
+  }
+}
